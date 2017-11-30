dcea0f93ead0ad91ff8c3cae55a11a79e4dcb8cc
chipmuenk <mail@chipmuenk.de>
2017-11-30 10:59:12 +0100

    Add files from labGUI repo (deleted)

---
+++ instruments/myGUI.bat
@@ -0,0 +1,3 @@
+rem C:\Programme\WinPython\python-2.7.6\Lib\site-packages\PyQt4\uic\pyuic.py Fluke45_GUI.ui -o Fluke45_GUI.py
+python D:\WinPython-64bit-2.7.6.3\python-2.7.6.amd64\Lib\site-packages\PyQt4\uic\pyuic.py Fluke45_GUI.ui -o Fluke45_GUI.py
+
